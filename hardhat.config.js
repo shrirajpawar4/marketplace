04e8ae35d507e0de2f384a47f5bc1cb963de8576
Shriraj Pawar <68687869+shrirajpawar4@users.noreply.github.com>
2022-03-06 15:49:53 +0530

    sab final

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,13 +19,25 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
  module.exports = {
   solidity: "0.8.4",
+  defaultNetwork: "rinkeby",
   networks: {
     hardhat: {},
     rinkeby: {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_KEY}`,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 20000,
+  },
+  gas: 2100000,
+  gasPrice: 8000000000,
 };
 
 
